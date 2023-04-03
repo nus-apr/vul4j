d5b696ea2e9bbe25e256dea9edeeb975d152899e
skark4ce <sirbusandu98@gmail.com>
2023-04-03 16:58:15 +0800

    - temporarily fix bug

--- base/init_dependencies.sh
+++ base/init_dependencies.sh
@@ -11,10 +11,15 @@ DEPENDENCY_PATH=$TARGET_PATH/dependency
 EXTRACTED_DEPENDENCIES_PATH=$TARGET_PATH/extracted_dependencies
 
 rm -rf "$EXTRACTED_DEPENDENCIES_PATH" \
-        && mkdir "$EXTRACTED_DEPENDENCIES_PATH" \
-                && cd "$EXTRACTED_DEPENDENCIES_PATH" \
-                        && echo "Extract all dependencies from the 'jar' files from $DEPENDENCY_PATH ..." \
-                                && find "$DEPENDENCY_PATH" -type f -name "*.jar" | awk '{print "jar -xf "$1}' | sh  \
-                                        && echo "Merge all dependencies into a single 'jar' file..." \
-                                                && jar -cvf "$TARGET_PATH"/all-dependencies.jar . \
-                                                        && echo "JAR File with all dependencies successfully created: $TARGET_PATH/all-dependencies.jar"
+  && mkdir "$EXTRACTED_DEPENDENCIES_PATH" \
+    && cd "$EXTRACTED_DEPENDENCIES_PATH" || exit 1;
+
+echo "Extract all dependencies from the 'jar' files from $DEPENDENCY_PATH ..." \
+    && find "$DEPENDENCY_PATH" -type f -name "*.jar" \(-exec jar -xf {} \; -o -quit\)
+
+echo "Delete *.RSA *.DSA *.SF signature files..." \
+  && find . -type f \( -name "*.RSA" -o -name "*.DSA" -o -name "*.SF" \) -delete -o -quit;
+
+echo "Merge all dependencies into a single 'jar' file..." \
+  && jar -cvf "$TARGET_PATH"/all-dependencies.jar . \
+    && echo "JAR File with all dependencies successfully created: $TARGET_PATH/all-dependencies.jar"
