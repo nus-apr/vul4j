db6dcb4a3f16d2f2b5571fd8a1b0cb7cf9e34100
Martin Mirchev <mmirchev@comp.nus.edu.sg>
2024-02-07 15:54:08 +0800

    Update paths

--- format_updater.py
+++ format_updater.py
@@ -7,7 +7,7 @@ x.close()
 for entry in contents:
     entry["language"] = "java"
     entry["src"] = {
-        "root_abspath": "/experiment/{subject}/{bug_id}/src".format(
+        "root_abspath": "/experiment/vul4j/{subject}/{bug_id}/src".format(
             subject=entry["subject"], bug_id=entry["bug_id"]
         ),
         "entrypoint": {
